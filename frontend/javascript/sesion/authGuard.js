98b2453fdbda3ea76a41ee8b1eb7fbd924c904f7
Ainoa Lopez <ainoalopezlizana@gmail.com>
2026-02-26 13:54:18 +0100

    mas login

--- frontend/javascript/sesion/authGuard.js
+++ frontend/javascript/sesion/authGuard.js
@@ -10,6 +10,12 @@ if (!token) {
     try {
         const payload = JSON.parse(atob(token.split('.')[1]));
         console.log('Payload JWT:', payload);
+        //guardar el correo en sessionStorage para mostrarlo en el header
+        sessionStorage.setItem("correo", payload.email);
+        //guardar el rol en sessionStorage para mostrarlo en el header
+        sessionStorage.setItem("rol", payload.rol);
+        // guardar id_usuario en sessionStorage para usarlo en las reservas
+        sessionStorage.setItem("id_usuario", payload.id_usuario);
 
         if (payload.exp && Date.now() >= payload.exp * 1000) {
             console.warn('Token expirado');
