1e78f0b7aee7139620d5d5a0049ad86c3c223889
ASdP13 <asdp20012006@gmail.com>
2026-04-19 19:07:03 +0200

    Servicio para el mailer

--- API/src/Controllers/ReservaEspacioController.php
+++ API/src/Controllers/ReservaEspacioController.php
@@ -8,6 +8,7 @@ use Core\Response;
 use Services\ReservaEspacioService;
 use Services\NecesidadReservaService;
 use Services\LogAccionesService;
+use Services\MailService;
 use Throwable;
 use Validation\ValidationException;
 
@@ -16,12 +17,14 @@ class ReservaEspacioController
     private ReservaEspacioService $service;
     private NecesidadReservaService $serviceNecesidad;
     private LogAccionesService $serviceLog;
+    private MailService $serviceMail;
 
     public function __construct()
     {
         $this->service = new ReservaEspacioService();
         $this->serviceNecesidad = new NecesidadReservaService();
         $this->serviceLog = new LogAccionesService();
+        $this->serviceMail = new MailService();
     }
 
     public function index(Request $req, Response $res): void
@@ -62,12 +65,16 @@ class ReservaEspacioController
     {
         try {
             $data = $req->getBody();
+            if(!$data['correo']){
+                throw new \Exception('Error al crear la reserva');
+            }
             $log['id_usuario_actor']=$data['id_usuario'];
             $reserva = $this->service->createReserva($data);
             if($data['necesidades']){
                 $this->serviceNecesidad->updateNecesidad((int)$reserva['id_reserva'], $data);
             }
             $log['id_reserva']=$reserva['id_reserva'];
+            $this->serviceMail->createMail($data['correo'], 'una creación de reserva', 'Creación de reserva');
             $this->serviceLog->createLog("Creación de reserva", $log);
             if(count($this->serviceNecesidad->getNecesidadById((int)$reserva['id_reserva']))>0){
                 $this->serviceLog->createLog("Asignación de necesidades", $log);
