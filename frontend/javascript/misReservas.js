beb1cfddad7ea6adaad2eb64029569ad7b8ca1e3
ASdP.13 <asdp20012006@gmail.com>
2026-05-08 14:29:12 +0200

    Ver necesidades y espacio en uso en reservas

--- frontend/javascript/misReservas.js
+++ frontend/javascript/misReservas.js
@@ -171,7 +171,7 @@ function mostrarVerDatosModal(reserva){
         let necesidades=[];
 
         if(typeof reserva.necesidades === "string"){
-            necesidades = reserva.necesidades ? reserva.necesidades.split(',').map(n => n.trim()) : [];
+            necesidades = reserva.nombrenecesidades ? reserva.nombrenecesidades.split(',').map(n => n.trim()) : [];
         }
 
         if(!necesidades||necesidades.length==0){
@@ -208,7 +208,7 @@ function mostrarVerDatosModal(reserva){
     document.getElementById("ver_reserva_espacio_uso").textContent = reserva.usaenespacio ?? '-';
     document.getElementById("ver_reserva_actividad").textContent = reserva.actividad ?? '-';
     document.getElementById("ver_reserva_observaciones").textContent = reserva.observaciones ?? '-';
-                    
+    console.log(reserva);
     if (reserva.tipo == 'Reserva_espacio') {
         document.getElementById("p_ver_unidades").classList.add('d-none');
         document.getElementById("reserva_unidades").required = false;
